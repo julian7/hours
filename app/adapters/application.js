02f4abe173b7de1b337a1d005a6c68ae06c15ce9
Balazs Nagy <julsevern@gmail.com>
2017-02-21 09:01:30 +0100

    Enable API server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+  host: 'http://127.0.0.1:4000'
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,5 @@
 export default function() {
+  this.passthrough('http://127.0.0.1:4000/api/**');
   this.namespace = '/api';
   this.resource('clients');
   this.resource('projects');
